eefe5a44c49703843ae782f0972bb0d356567d91
AtlasWongy <atlasyjyj@gmail.com>
2025-11-06 10:35:12 +0800

    Implementing state and state machine

---
+++ state_machine/state.gd
@@ -0,0 +1,22 @@
+extends Node2D
+class_name State
+
+@onready var debug = owner.find_child("Debug")
+#@onready var player = owner.get_parent().find_child("player")
+@onready var animation_player = owner.find_child("AnimationPlayer")
+
+func _ready() -> void:
+	set_physics_process(false)
+
+func enter() -> void:
+	set_physics_process(true)
+
+func exit() -> void:
+	set_physics_process(false)
+
+func transition() -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	transition()
+	debug.text = name
